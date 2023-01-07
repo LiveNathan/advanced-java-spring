13ac0642d9281e4ea4262afbe673c69cf7d9b41b
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-07 17:44:07 -0600

    practicing @Import

--- src/main/java/platform/codingnomads/co/corespring/examples/importannotation/Love.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/importannotation/Love.java
@@ -2,13 +2,11 @@ package platform.codingnomads.co.corespring.examples.importannotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({SimpleConfiguration.class, Love.class})
-public class ImportAnnotationConfig {
+public class Love {
     @Bean
-    public Framework framework() {
-        return new Framework();
+    public SpringDeveloper springDeveloper2() {
+        return new SpringDeveloper();
     }
 }
