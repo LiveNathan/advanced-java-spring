5d869f4446be671821d2cc1eab5846245aab2c62
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-07 18:03:03 -0600

    fixed bug with too many beans of same type

--- src/main/java/platform/codingnomads/co/corespring/examples/importannotation/Love.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/importannotation/Love.java
@@ -2,10 +2,12 @@ package platform.codingnomads.co.corespring.examples.importannotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class Love {
     @Bean
+    @Primary
     public SpringDeveloper springDeveloper2() {
         return new SpringDeveloper();
     }
