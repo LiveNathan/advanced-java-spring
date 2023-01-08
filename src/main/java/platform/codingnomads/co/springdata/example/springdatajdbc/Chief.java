9f9d508891b98ae4e0ef24736ac54c53e4b12707
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-08 16:17:50 -0600

    learning about JdbcTemplate

---
+++ src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/Chief.java
@@ -0,0 +1,11 @@
+package platform.codingnomads.co.springdata.example.springdatajdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Chief {
+    private long id;
+    private String block;
+}
--- src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
@@ -30,15 +30,30 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
         } catch (Exception e) {
             //nothing
         }
+        try {
+            //create Chief table using the JdbcTemplate method "execute"
+            jdbcTemplate.execute("CREATE TABLE chief (id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+                    "block VARCHAR(255) NOT NULL);");
+        } catch (Exception e) {
+            //nothing
+        }
 
         //create a list of first and last names
         List<Object[]> splitUpNames = Stream.of("Java Ninja", "Spring Guru", "Java Guru", "Spring Ninja")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+        //create a list of random words
+        List<Object[]> randomWords = Stream.of("joint", "game", "tile", "fog", "foggy")
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
 
         //for each first & last name pair insert an Employee into the database
         for (Object[] name : splitUpNames) {
             jdbcTemplate.execute(String.format("INSERT INTO employees(first_name, last_name) VALUES ('%s','%s')", name[0], name[1]));
+        }
+        //for each random word insert a chief into the database
+        for (Object[] word : randomWords) {
+            jdbcTemplate.execute(String.format("INSERT INTO chief(block) VALUES ('%s')", word[0]));
         }
 
         //query the database for Employees with first name Java
@@ -48,10 +63,19 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
                 )
                 //print each found employee to the console
                 .forEach(employee -> System.out.println(employee.toString()));
+        //query the database for chief ordered ascending
+        jdbcTemplate.query(
+                        "SELECT id, block FROM chief ORDER BY block",
+                        (rs, rowNum) -> new Chief(rs.getLong("id"), rs.getString("block"))
+                )
+                //print each found chief to the console
+                .forEach(chief -> System.out.println(chief.toString()));
 
         //truncate the table
         jdbcTemplate.execute("TRUNCATE TABLE employees;");
+        jdbcTemplate.execute("TRUNCATE TABLE chief;");
         //delete the table
         jdbcTemplate.execute("DROP TABLE employees");
+        jdbcTemplate.execute("DROP TABLE chief");
     }
 }
