60115100b3ab9f164fd5eba887ef3cca655665f9
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-12 11:34:37 -0600

    trying to figure out how to return a single Coke Zero object

--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
@@ -5,9 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
+import java.util.List;
 
 @SpringBootApplication
 public class CrudRepoDemo implements CommandLineRunner {
@@ -25,10 +23,13 @@ public class CrudRepoDemo implements CommandLineRunner {
         //create new user
         User user = User.builder().firstName("Bobby").lastName("Bobbert").age(56).build();
         User user2 = User.builder().firstName("Joanne").lastName("Joanna").age(36).build();
+        User user1 = User.builder().firstName("Nathan").lastName("Lively").age(42).build();
+        User user3 = User.builder().firstName("Elis").lastName("Bradshaw").age(41).build();
 
         //save user and assign what is returned to the user variable.
-        user = userRepo.save(user);
-        user2 = userRepo.save(user2);
+//        user = userRepo.save(user);
+//        user2 = userRepo.save(user2);
+        List<User> userList = (List<User>) userRepo.saveAll(List.of(user, user1, user2, user3));
 
         Iterable<User> users = userRepo.findAll();
 
--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/jparepository/JpaRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/jparepository/JpaRepoDemo.java
@@ -23,28 +23,39 @@ public class JpaRepoDemo implements CommandLineRunner {
         SoftDrink fanta = SoftDrink.builder().name("Fanta").rating(10).build();
         SoftDrink coke = SoftDrink.builder().name("Coca-Cola").rating(4).build();
         SoftDrink drPepper = SoftDrink.builder().name("Dr. Pepper").rating(1).build();
+        SoftDrink sprite = SoftDrink.builder().name("Sprite").rating(7).build();
+        SoftDrink cokeZero = SoftDrink.builder().name("Coke Zero").rating(6).build();
+        List<SoftDrink> softDrinks = List.of(coke,fanta,drPepper,sprite, cokeZero);
 
         //save single entity instance
-        fanta = softDrinkRepo.save(fanta);
+//        fanta = softDrinkRepo.save(fanta);
 
         //save multiple entity instances at a time
-        List<SoftDrink> insertedSoftDrinks = softDrinkRepo.saveAll(List.of(coke, drPepper));
+//        List<SoftDrink> insertedSoftDrinks = softDrinkRepo.saveAll(List.of(coke, drPepper));
+        List<SoftDrink> insertedSoftDrinks = softDrinkRepo.saveAll(softDrinks);
 
-        //make sure all entities are actually saved to the database
+        // make sure all entities are actually saved to the database
         softDrinkRepo.flush();
 
-        //update coke and drPepper to have rating 0 in the database
-        for (SoftDrink sd : insertedSoftDrinks) {
-            sd.setRating(0);
-            softDrinkRepo.save(sd);
-        }
+        // update coke and drPepper to have rating 0 in the database
+//        for (SoftDrink sd : insertedSoftDrinks) {
+//            sd.setRating(0);
+//            softDrinkRepo.save(sd);
+//        }
+        insertedSoftDrinks.get(0).setRating(0);
+        insertedSoftDrinks.get(2).setRating(0);
+        insertedSoftDrinks = softDrinkRepo.saveAll(softDrinks);
 
         System.out.println("ALL SOFT DRINKS IN DESCENDING ORDER BASED ON ID");
         //get all soft drinks in ascending order and print toString() to the console
         softDrinkRepo.findAll(Sort.by(Sort.Direction.DESC, "id")).forEach(System.out::println);
 
+        System.out.println("ALL SOFT DRINKS IN DESCENDING ORDER BASED ON RATING");
+        //get all soft drinks in ascending order and print toString() to the console
+        softDrinkRepo.findAll(Sort.by(Sort.Direction.DESC, "rating")).forEach(System.out::println);
+
         //find all using an example
-        System.out.println("FINDING ALL USING EXAMPLE");
+        System.out.println("FIND ALL USING EXAMPLE");
         softDrinkRepo.findAll(
                         Example.of(
                                 //probe soft drink to match results with
@@ -53,6 +64,13 @@ public class JpaRepoDemo implements CommandLineRunner {
                                 ExampleMatcher.matchingAny())
                 )
                 .forEach(System.out::println);
+
+        // find CokeZero
+        System.out.println("FIND COKE ZERO");
+        Example<SoftDrink> example = Example.of(SoftDrink.builder().name("Coke Zero").build(), ExampleMatcher.matchingAny());
+        List<SoftDrink> foundCokeZero = softDrinkRepo.findAll(example);
+        System.out.println(foundCokeZero);
+//        softDrinkRepo.findAll(example).forEach(System.out::println);
 
         //create page request to paginate through these 3 soft drinks. note that the first page is indicated using a 0
         PageRequest pageRequest = PageRequest.of(0, 2);
