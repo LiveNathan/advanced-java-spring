2d7e70e680a3ca40d3b9904a61e2aa018e8b6614
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-22 17:14:36 -0600

    learning about patchForObject

--- src/main/java/platform/codingnomads/co/springweb/resttemplate/PATCH/PatchMain.java
+++ src/main/java/platform/codingnomads/co/springweb/resttemplate/PATCH/PatchMain.java
@@ -1,7 +1,5 @@
 package platform.codingnomads.co.springweb.resttemplate.PATCH;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import platform.codingnomads.co.springweb.resttemplate.PATCH.models.ResponseObject;
-import platform.codingnomads.co.springweb.resttemplate.PATCH.models.Task;
+import platform.codingnomads.co.springweb.resttemplate.POST.postForObject.Models.UserResponseObject;
+import platform.codingnomads.co.springweb.resttemplate.POST.postForObject.Models.UserTemplate;
 
 import java.util.Objects;
 
@@ -30,12 +27,12 @@ public class PatchMain {
     @Bean
     public CommandLineRunner run() throws Exception {
         return args -> {
-
+            /* DONE
             //create an empty Task
             Task task = new Task();
 
             //be sure to use a valid task id
-            task.setId(169);
+            task.setId(234);
 
             //set fields you want to change. All other fields are null and will not be updated
             task.setName("use patchForObject()");
@@ -53,6 +50,28 @@ public class PatchMain {
             HttpEntity<Task> httpEntity = new HttpEntity<>(task);
             ResponseEntity<ResponseObject> response = restTemplate
                     .exchange("http://demo.codingnomads.co:8080/tasks_api/tasks/" + task.getId(), HttpMethod.PATCH, httpEntity, ResponseObject.class);
+
+            System.out.println(Objects.requireNonNull(response)); */
+            // Create an empty User
+            UserTemplate userTemplate = UserTemplate.builder().build();
+
+            // Use a valid User id
+            userTemplate.setId(479);
+
+            // Set fields. All other fields are null and will not be updated.
+            userTemplate.setFirst_name("use patchForObject()");
+            userTemplate.setLast_name("this lastName was updated using patchForObject()");
+
+            // Send the PATCH request using the URL, the User created above and the UserResponseObject Class
+            UserResponseObject objectResponse = restTemplate.patchForObject("http://demo.codingnomads.co:8080/tasks_api/users/" + userTemplate.getId(), userTemplate, UserResponseObject.class);
+
+            System.out.println(Objects.requireNonNull(objectResponse));
+
+            userTemplate.setFirst_name("PATCH using exchange()");
+            userTemplate.setLast_name("This user was updated using PATCH");
+
+            HttpEntity<UserTemplate> httpEntity = new HttpEntity<>(userTemplate);
+            ResponseEntity<UserResponseObject> response = restTemplate.exchange("http://demo.codingnomads.co:8080/tasks_api/users/" + userTemplate.getId(), HttpMethod.PATCH, httpEntity, UserResponseObject.class);
 
             System.out.println(Objects.requireNonNull(response));
         };
--- src/main/java/platform/codingnomads/co/springweb/resttemplate/POST/postForObject/Models/UserResponseObject.java
+++ src/main/java/platform/codingnomads/co/springweb/resttemplate/POST/postForObject/Models/UserResponseObject.java
@@ -1,8 +1,10 @@
 package platform.codingnomads.co.springweb.resttemplate.POST.postForObject.Models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UserResponseObject {
     UserTemplate data;
     ErrorTemplate error;
