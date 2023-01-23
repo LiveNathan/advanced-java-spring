e67664a79ab5600aeb8eb08eac192b02b24e21ab
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-23 10:32:09 -0600

    learning about @RequestMapping

--- src/main/java/platform/codingnomads/co/springweb/springrestcontrollers/simpledemo/controller/HelloWorldController.java
+++ src/main/java/platform/codingnomads/co/springweb/springrestcontrollers/simpledemo/controller/HelloWorldController.java
@@ -2,6 +2,9 @@ package platform.codingnomads.co.springweb.springrestcontrollers.simpledemo.cont
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import platform.codingnomads.co.springweb.resttemplate.PUT.models.Task;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -15,6 +18,18 @@ public class HelloWorldController {
     @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String greeting(@PathVariable(name = "name") String name) {
         return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/hello/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String helloList() {
+        List<Integer> integerList = List.of(1, 2, 3, 4);
+        return integerList.toString();
+    }
+
+    @RequestMapping(path = "/hello/pojo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String helloTask() {
+        Task task = Task.builder().userId(479).name("Build bars.").description("So many bars.").completed(false).build();
+        return task.toString();
     }
 }
 
