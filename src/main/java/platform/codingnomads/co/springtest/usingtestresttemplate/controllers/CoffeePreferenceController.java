08930ac9d3b5ee93bea49d19dc97e8bfd8a675b4
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-09 10:52:13 -0600

    learning about usingtestresttemplate

--- src/main/java/platform/codingnomads/co/springtest/usingtestresttemplate/controllers/CoffeePreferenceController.java
+++ src/main/java/platform/codingnomads/co/springtest/usingtestresttemplate/controllers/CoffeePreferenceController.java
@@ -3,9 +3,7 @@ package platform.codingnomads.co.springtest.usingtestresttemplate.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import platform.codingnomads.co.springtest.usingtestresttemplate.models.CoffeePreference;
 import platform.codingnomads.co.springtest.usingtestresttemplate.services.CoffeePreferenceService;
 
@@ -23,6 +21,16 @@ public class CoffeePreferenceController {
             return ResponseEntity.ok().header("Location", "http://www.url.com/new/location").body(preference);
         } catch (Exception e) {
             return ResponseEntity.status(500).body(new Exception(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCoffeePreference(@PathVariable Long id) {
+        try {
+            CoffeePreference preference = service.getCoffeePreferenceById(id);
+            return ResponseEntity.ok().body(preference);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new Exception(e.getMessage()));
         }
     }
 }
